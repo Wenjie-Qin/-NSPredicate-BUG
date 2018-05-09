ad4e06727711aa17242c8ae56ec7422ebc31d6f3
Wenjie-Qin <1033856047@qq.com>
2018-05-09 15:00:17 +0800

    Add files via upload

---
+++ WJTestDemo.h
@@ -0,0 +1,18 @@
+//
+//  WJTestDemo.h
+//  BUGTest
+//
+//  Created by Mac on 2018/5/9.
+//  Copyright © 2018年 Mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WJTestDemo : NSObject
+
++ (BOOL)string:(NSString *)str;
+
+//  奇葩空格符的BUG
++ (void)errorStr;
+
+@end
